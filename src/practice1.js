8015527d70af7bb3bbcba39c322e759dcf67f5f1
zuowen.Wang <798389920@qq.com>
2020-09-11 20:04:48 +0800

    finish all

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,8 @@
-const parseData
+const parseData = ({data,column}) =>{
+    let [{ name: name},{ name: age },{ name: gender },{ name: birthday}] = column;
+    let result = []
+    data.map(value => {let [nameValue, ageValue, genderValue, birthdayValue] = value;let unit = {name:nameValue,age:ageValue,gender:genderValue,birthday:birthdayValue};
+        result.push(unit)});
+    return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = (items,sections) =>{
+    let indexBefore = 0;
+    sections.map(section => {let { content: value, index: index } = section;
+        if(index>indexBefore){
+            items.splice(index+1,0,value);
+            indexBefore = index;
+        }else{
+            items.splice(index,0,value);
+        }})
+    return items;
+}
 export { inject };
