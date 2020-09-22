fe8cb9a4ef28bdce89c59fa300f21e2cb08f7489
kevindongzg <dongkevin94@gmail.com>
2020-09-22 17:58:25 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,7 +1,11 @@
 const parseData = ({data,column}) =>{
+    // TODO 这里column的长度，key在哪一位不是确定的，后端的返回可能有变化，应该是采用index匹配key-value的形式而不是写死
     let [{ name: name},{ name: age },{ name: gender },{ name: birthday}] = column;
     let result = []
+    // TODO 使用map做数组与数组的转化，不用声明新数组
+    // TODO 注意换行
     data.map(value => {let [nameValue, ageValue, genderValue, birthdayValue] = value;
+        // TODO 这里可以使用对象的简洁表示法
         let unit = {name:nameValue,age:ageValue,gender:genderValue,birthday:birthdayValue};
         result.push(unit)});
     return result;
--- src/practice2.js
+++ src/practice2.js
@@ -1,6 +1,9 @@
 const inject = (items,sections) =>{
+    // TODO 未使用的变量 indexBefore
     let indexBefore = 0;
     let i = 0;
+    // TODO 注意换行与缩进
+    // TODO 不是map的使用场景，且map/forEach内函数第二个参数就是index，不用重新声明i
     sections.sort((a,b) => a.index - b.index).map(section => {let { content: value, index: index } = section;
         items.splice(index+i,0,value);
         i++;
